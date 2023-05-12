db25484ed599d6208fee735221c94fed58498d0e
Darmatage <JasonWiserArt@gmail.com>
2023-05-12 14:31:26 -0400

    Revisions to level 4: space tightening

--- DeepDark_game/Assets/Scripts/Drill.cs
+++ DeepDark_game/Assets/Scripts/Drill.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 
 public class Drill : MonoBehaviour{
 
-      public GameHandler gameHandler;
-      public GameObject DrillOn;
-      public GameObject DrillOff;
-public AudioSource DrillSFX;
+	public GameHandler gameHandler;
+	public GameObject DrillOn;
+	public GameObject DrillOff;
+	public AudioSource DrillSFX;
+	public GameObject damageCollider;
 
       void Start(){
             gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
             DrillOn.SetActive(true);
             DrillOff.SetActive(false);
+			damageCollider.SetActive(true);
 			DrillSFX.Play();
       }
 
       public void OnTriggerEnter2D (Collider2D other){
             if(other.gameObject.tag == "Player"){
                   if (GameInventory.item6num > 2) {
+						GetComponent<NPCDialogue>().enabled = false;
                         DrillOn.SetActive(false);
                         DrillOff.SetActive(true);
+						damageCollider.SetActive(false);
 						DrillSFX.Stop();
-                        gameHandler.GetComponent<GameInventory>().InventoryRemove("item6", 1);
+                        gameHandler.GetComponent<GameInventory>().InventoryRemove("item6", 3);
+						
                   }
                   else {
-                        Debug.Log("You need a key to enter.");
+                        Debug.Log("You need 3 keys to shut down the drill.");
                   }
             }
       }
